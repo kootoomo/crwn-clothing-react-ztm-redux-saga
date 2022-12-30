88cf8c29e97c873fa0abeb97266d45c0e6607305
koo <kootoomo@gmail.com>
2022-12-30 06:01:10 +0300

    re-do with redux-saga

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import { useEffect } from 'react';
+import { Routes, Route } from "react-router-dom";
+import { useDispatch } from 'react-redux';
+
+import Home from "./routes/home/home.component.jsx";
+import Navigation from "./routes/navigation/navigation.component.jsx";
+import Authentication from "./routes/authentication/authentication.component";
+import Shop from "./routes/shop/shop.component.jsx";
+import Checkout from "./routes/checkout/checkout.component.jsx";
+
+import { checkUserSession } from './store/user/user.action';
+
+const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+   dispatch(checkUserSession());
+  }, []);
+
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop/*" element={<Shop />} />
+        <Route path="auth" element={<Authentication />} />
+        <Route path="checkout" element={<Checkout />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default App;
